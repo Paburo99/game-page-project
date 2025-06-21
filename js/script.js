de4c06bfbce28e1fc45363ba8c77db541f205365
Paburo <paburo99@gmail.com>
2025-06-20 23:07:20 -0500

    Mejoras visuales y de animación

--- js/script.js
+++ js/script.js
@@ -1,88 +1,94 @@
 // -- COUNTDOWN TIMER --
-const daysElement = document.getElementById("days");
-const hoursElement = document.getElementById("hours");
-const minutesElement = document.getElementById("minutes");
-const secondsElement = document.getElementById("seconds");
+function initCountdown() {
+    const daysElement = document.getElementById("days");
+    const hoursElement = document.getElementById("hours");
+    const minutesElement = document.getElementById("minutes");
+    const secondsElement = document.getElementById("seconds");
 
-const releaseDate = new Date("2025-07-16T20:00:00");
+    const releaseDate = new Date("2025-07-16T20:00:00");
 
-function updateCountdown() {
-    const now = new Date();
-    const timeDifference = releaseDate.getTime() - now.getTime(); //milliseconds
+    function updateCountdown() {
+        const now = new Date();
+        const timeDifference = releaseDate.getTime() - now.getTime(); //milliseconds
 
-    if (timeDifference > 0) {
-        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        if (timeDifference > 0) {
+            const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-        daysElement.textContent = String(days).padStart(2, "0");
-        hoursElement.textContent = String(hours).padStart(2, "0");
-        minutesElement.textContent = String(minutes).padStart(2, "0");
-        secondsElement.textContent = String(seconds).padStart(2, "0");
-    } else {
-        clearInterval(timerInterval);
-        document.querySelector(".countdown-timer").innerHTML = "<h3>The Game is Out Now!</h3>";
-    }
-}
-
-const timerInterval = setInterval(updateCountdown, 1000);
-
-// -- SMOOTH SCROLLING --
-const nav = document.querySelector("header nav");
-
-nav.addEventListener("click", (event) => {
-    if (event.target.tagName === "A") {
-        event.preventDefault();
-
-        const href = event.target.getAttribute("href");
-        const targetSection = document.querySelector(href);
-
-        if (targetSection) {
-            targetSection.scrollIntoView({
-                behavior: "smooth"
-            });
+            daysElement.textContent = String(days).padStart(2, "0");
+            hoursElement.textContent = String(hours).padStart(2, "0");
+            minutesElement.textContent = String(minutes).padStart(2, "0");
+            secondsElement.textContent = String(seconds).padStart(2, "0");
+        } else {
+            clearInterval(timerInterval);
+            document.querySelector(".countdown-timer").innerHTML = "<h3>The Game is Out Now!</h3>";
         }
     }
-})
+
+    const timerInterval = setInterval(updateCountdown, 1000);
+}
+
+// -- SMOOTH SCROLLING --
+function initSmoothScrolling() {
+    const nav = document.querySelector("header nav");
+
+    nav.addEventListener("click", (event) => {
+        if (event.target.tagName === "A") {
+            event.preventDefault();
+
+            const href = event.target.getAttribute("href");
+            const targetSection = document.querySelector(href);
+
+            if (targetSection) {
+                targetSection.scrollIntoView({
+                    behavior: "smooth"
+                });
+            }
+        }
+    });
+}
 
 // -- VIDEO MODAL --
-const playVideoBtn = document.getElementById("btnPlayVideo");
-const videoModal = document.getElementById("videoModal");
-const videoPlayer = document.getElementById("videoPlayer");
-const btnClose = document.getElementById("btnClose");
+function initVideoModal() {
+    const playVideoBtn = document.getElementById("btnPlayVideo");
+    const videoModal = document.getElementById("videoModal");
+    const videoPlayer = document.getElementById("videoPlayer");
+    const btnClose = document.getElementById("btnClose");
 
-function openModal() {
-    videoModal.classList.add("active");
-    document.body.classList.add("modal-open");
-    videoPlayer.play();
-}
-
-function closeModal() {
-    videoModal.classList.remove("active");
-    document.body.classList.remove("modal-open");
-    videoPlayer.pause();
-}
-
-// Event listeners
-btnPlayVideo.addEventListener("click", openModal);
-btnClose.addEventListener("click", closeModal);
-
-videoModal.addEventListener("click", (e) => {
-    if (e.target === videoModal) {
-        closeModal();
+    function openModal() {
+        videoModal.classList.add("active");
+        document.body.classList.add("modal-open");
+        videoPlayer.play();
     }
-});
 
-document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape" && videoModal.classList.contains("active")) {
-        closeModal();
+    function closeModal() {
+        videoModal.classList.remove("active");
+        document.body.classList.remove("modal-open");
+        videoPlayer.pause();
     }
-});
 
-document.querySelector(".video-container").addEventListener("click", (e) => {
-    e.stopPropagation();
-});
+    // Event listeners
+    playVideoBtn.addEventListener("click", openModal);
+    btnClose.addEventListener("click", closeModal);
+
+    videoModal.addEventListener("click", (e) => {
+        if (e.target === videoModal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener("keydown", (e) => {
+        if (e.key === "Escape" && videoModal.classList.contains("active")) {
+            closeModal();
+        }
+    });
+
+    document.querySelector(".video-container").addEventListener("click", (e) => {
+        e.stopPropagation();
+    });
+}
 
 // -- GALLERY FUNCTIOANLITY --
 class Gallery {
@@ -343,21 +349,13 @@ function initMobileMenu() {
 
 // -- ANIMATION --
 function initAnimations() {
-    /*     gsap.fromTo('.logo',
-            {
-                opacity: 1,
-                scale: 1
-            },
-            {
-                duration: 1.5,
-                opacity: 0,
-                scale: 0.75,
-                ease: CustomWiggle.create('myWiggle', {
-                    wiggles: 5,
-                    type: 'easeOut'
-                })
-            }); */
 
+    const heroTitle = document.querySelector('.hero-content h1');
+    const splitHeroTitle = new SplitText(heroTitle, { type: "chars" });
+    const heroText = document.querySelector('.hero-content p');
+    const splitHeroText = new SplitText(heroText, { type: "chars" });
+
+    /* Header and Hero Animation Timeline */
     const timeline = gsap.timeline({ defaults: { opacity: 0, ease: 'power2.out', duration: 0.8 } });
 
     timeline
@@ -371,12 +369,34 @@ function initAnimations() {
             y: '-2rem',
             stagger: 0.15,
         }, '<')
-        .from('#hero .hero-content h1', {
-            y: '-2rem',
-        }, '<0.2')
-        .from('#hero .hero-content p', {
-            y: '-2rem',
-        }, '<0.2')
+        .from(splitHeroTitle.chars, {
+            duration: 1,
+            opacity: 0,
+            y: 'random(-20rem, 20rem)',
+            rotation: 'random(-30deg, 30deg)',
+            ease: "power2.out",
+            stagger: 0.15,
+            /* onComplete: () => {
+                splitHeroTitle.chars.forEach(char => {
+                    gsap.to(char, {
+                        y: 'random(-1.5rem, 1.5rem)',
+                        rotation: 'random(-5, 5)',
+                        duration: 'random(3, 4)',
+                        ease: 'sine.inOut',
+                        repeat: -1,
+                        yoyo: true
+                    });
+                });
+            } */
+        }, '<')
+        .from(splitHeroText.chars, {
+            duration: 0.5,
+            opacity: 0,
+            y: 'random(-20rem, 20rem)',
+            rotation: 'random(-30deg, 30deg)',
+            ease: "power2.out",
+            stagger: 0.05,
+        }, '<')
         .from('#hero .hero-content .btn-play-video', {
             y: '-2rem',
         }, '<0.2')
@@ -391,6 +411,24 @@ function initAnimations() {
             y: '-2rem',
         }, '<0.2');
 
+    /* Section Scroll Trigger */
+    const sections = document.querySelectorAll('section:not(#hero, #features)');
+
+    sections.forEach(section => {
+        gsap.from(section, {
+            scrollTrigger: {
+                trigger: section,
+                start: 'top 80%',
+                end: 'bottom 90%',
+            },
+            opacity: 0,
+            y: '10rem',
+            duration: 1,
+            ease: 'power3.out'
+        });
+    });
+
+    /* Special Scroll Trigger for Features Section */
     /* Scrubbed animation for smooth scroll-based movement */
     gsap.from('.feature-card', {
         scrollTrigger: {
@@ -413,7 +451,6 @@ function initAnimations() {
             start: 'top 90%',
             end: 'bottom 10%',
             toggleActions: 'play reverse play reverse',
-            markers: true
         },
         opacity: 0,
         duration: 0.8,
@@ -422,56 +459,59 @@ function initAnimations() {
             from: 'start'
         },
     });
+
+    /* Features Card Animation */
+    const featuresCard = document.querySelectorAll('.feature-card');
+    const featuresGrid = document.querySelector('.features-grid');
+
+    gsap.set(featuresGrid, { perspective: '100rem' });
+    gsap.set(featuresCard, { transformStyle: 'preserve-3d' });
+
+    featuresCard.forEach(card => {
+        card.addEventListener("mousemove", e => {
+            const rect = e.currentTarget.getBoundingClientRect();
+
+            // Position relative to the center of the card (-0.5 to 0.5)
+            const x = (e.clientX - rect.left) / rect.width - 0.5;
+            const y = (e.clientY - rect.top) / rect.height - 0.5;
+            const z = (x * y);
+
+            // Map mouse position to rotation angle
+            const maxTilt = 10;
+            const tiltX = y * -maxTilt;
+            const tiltY = x * maxTilt;
+            const tiltZ = z * maxTilt;
+
+            gsap.to(card, {
+                rotateX: tiltX,
+                rotateY: tiltY,
+                rotateZ: tiltZ,
+                duration: 0.4,
+                ease: "power2.out",
+                overwrite: "auto"
+            });
+        });
+
+        card.addEventListener("mouseleave", () => {
+            gsap.to(card, {
+                rotateX: 0,
+                rotateY: 0,
+                rotateZ: 0,
+                duration: 0.8,
+                ease: 'elastic.out(1, 0.5)',
+                overwrite: 'auto'
+            });
+        });
+    });
+
 }
-
-const featuresCard = document.querySelectorAll('.feature-card');
-
-featuresCard.forEach(card => {
-    let isHovering = false;
-
-    const playLeaveAnimation = () => {
-        gsap.to(card, {
-            rotateX: 0,
-            duration: 0.8,
-            ease: 'power2.out',
-            overwrite: 'auto'
-        });
-    };
-
-    const playEnterAnimation = () => {
-        gsap.to(card, {
-            rotateX: 360,
-            duration: 0.8,
-            ease: 'power2.out',
-            onComplete: () => {
-                // If the mouse left while it was animating, play the leave animation now
-                if (!isHovering) {
-                    playLeaveAnimation();
-                }
-            },
-            overwrite: 'auto'
-        });
-    };
-
-    card.addEventListener("mouseenter", () => {
-        isHovering = true;
-        // Don't interrupt an ongoing animation
-        if (!gsap.isTweening(card)) {
-            playEnterAnimation();
-        }
-    });
-
-    card.addEventListener("mouseleave", () => {
-        isHovering = false;
-        // Don't interrupt an ongoing animation
-        if (!gsap.isTweening(card)) {
-            playLeaveAnimation();
-        }
-    });
-});
 
 // -- DOM INITIALIZATION --
 document.addEventListener('DOMContentLoaded', () => {
+    gsap.registerPlugin(Flip, ScrollTrigger, TextPlugin, SplitText, CustomEase, CustomWiggle);
+    initCountdown();
+    initSmoothScrolling();
+    initVideoModal();
     new Gallery();
     initMobileMenu();
     initAnimations();
